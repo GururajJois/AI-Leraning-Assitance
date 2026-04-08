da81208418c8c11ba135fe010b3cf0ff45d93263
GururajJois <gururajjoisclg@gmail.com>
2026-04-09 00:46:21 +0530

    started with flashcard api

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ import connectDB from './config/db.js';
 import errorHandler from './middleware/errorHandler.js';
 import authRoutes from './routes/authRoutes.js';
 import documentRoutes from './routes/documentRoutes.js';
+import flashcardRoutes from './routes/flashcardRoutes.js';
 
 
 //Es6 module __dirname alternative
@@ -41,6 +42,7 @@ app.use('/uploads',express.static(path.join(__dirname, 'uploads')));
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/documents', documentRoutes);
+app.use('/api/flashcards', flashcardRoutes);
 
 app.use(errorHandler);
 
